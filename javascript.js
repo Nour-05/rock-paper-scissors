bbc621e903cc29b84eea0d41c06546d524ccbf53
nour <140322843+Nour-05@users.noreply.github.com>
2026-09-06 12:53:17 +0300

    added game logic

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-console.log("Hello World");
-
 function getComputerChoice(){
     let rand = Math.floor(Math.random() * 3);
     if (rand === 0)
@@ -9,11 +7,36 @@ function getComputerChoice(){
     else return "scissors";
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice(){
     return prompt("Enter one of the choices(rock,paper, scissors)");
 
 }
 
-console.log(getHumanChoice());
+
+let computerScore = 0;
+let humanScore = 0;
+
+function playRound(humanChoice, computerChoice){
+
+    if(humanChoice === computerChoice)
+        console.log("Draw");
+    else if ((humanChoice==="rock" && computerChoice==="scissors") || (humanChoice==="scissors" && computerChoice==="paper") || (humanChoice==="paper" && computerChoice==="rock")){
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
+        humanScore++;
+    }
+    else{
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        computerScore++;
+    }
+}
+
+function playGame(){
+    
+    for(let i = 0; i < 5; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+    let winner = humanScore > computerScore ? "you" : "me";
+    console.log("Final winner is " + winner + " !!!!")
+}
